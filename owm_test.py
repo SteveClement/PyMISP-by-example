f3b9ac5929156528eca584d73ea5fa9ac00012f0
Steve Clement <steve@localhost.lu>
2018-01-18 17:18:08 +0100

    - small refactor and more moving around of superflous output to debug section

--- owm_test.py
+++ owm_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("This test script will make sure you have: \n - Internetz \n - A valid configuration file config/keys.py \n - The ability to get weather data for the current location\n\n\n")
 
 try:
     from config.keys import *
@@ -22,22 +25,40 @@ owm = pyowm.OWM(owm_key)  # You MUST provide a valid API key
 # Have a pro subscription? Then use:
 # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
 
-# Search for current weather in London (Great Britain)
+# To search for current weather in London (Great Britain): London,GB
+
+currLoc = j['country_name'] + ',' + j['country_code']
+
 try:
-    observation = owm.weather_at_place('London,GB')
+    if forceLoc: currLoc = forceLoc
+    observation = owm.weather_at_place(currLoc)
 except pyowm.exceptions.unauthorized_error.UnauthorizedError as e:
     print(f"Something went wrong:\n {e}")
     sys.exit(6)
 
-w = observation.get_weather()
-print(w)                      # <Weather - reference time=2013-12-18 09:20,
-                              # status=Clouds>
+w = observation.get_weather() # Type: pyowm.webapi25.weather.Weather - Docu: https://pyowm.readthedocs.io/en/latest/pyowm.webapi25.html
+
+if w.get_rain():
+    print("Current rain info: {}".format(w.get_rain()))
+if w.get_snow():
+    print("Current snow info: {}".format(w.get_snow()))
 
 # Weather details
-w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-w.get_humidity()              # 87
-w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+print(f"Weather details for {currLoc} - Test script\n\n\n")
+print("Current wind speed in km/h: {}".format(w.get_wind()['speed']*3.6)) # {'speed': 4.6, 'deg': 330}
+print("Current temperatur in degC: {}".format(w.get_temperature('celsius')['temp']))  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
-# Search current weather observations in the surroundings of
-# lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
-observation_list = owm.weather_around_coords(-22.57, -43.12)
+if DEBUG:
+    # Search current weather observations in the surroundings of
+    # lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
+    observation_list = owm.weather_around_coords(-22.57, -43.12)
+    print("####### ---> DEBUG OUTPUT <--- #######")
+    print(observation_list)
+    print("\n\n\n")
+    if j['city']:
+        print(j['city'].encode('iso-8859-1').decode())
+        print("\n\n\n")
+    print(f"Humidity for {currLoc}: {w.get_humidity()}")              # 87
+    print(f"Weather for {currLoc}: \n {w}") # <Weather - reference time=2013-12-18 09:20, status=Clouds>
+
+print(f"Actual JSON of our weather object: \n{w.to_JSON()}")
