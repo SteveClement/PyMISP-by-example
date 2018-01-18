46d2cfdf3f7e3a5546f00cab989f0be77c029be5
Steve Clement <steve@localhost.lu>
2018-01-18 10:42:38 +0100

    - Added tries and catches

--- owm_test.py
+++ owm_test.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
-from config.keys import *
+
+try:
+    from config.keys import *
+except ImportError:
+    print("Please make sure you have a valid config file in config/keys.py")
+
 import pyowm
-owm = pyowm.OWM('fc21e996d0d3c2ecdf17f800cca1e478')  # You MUST provide a valid API key
+import requests
+import json
+import sys
+
+try:
+    r = requests.get(freeGeoIP_url)
+    j = json.loads(r.text)
+except requests.exceptions.ConnectionError as e:
+    print(f"Some form of ConnectionError exception has occured: \n {e}")
+    sys.exit(4)
+
+owm = pyowm.OWM(owm_key)  # You MUST provide a valid API key
 
 # Have a pro subscription? Then use:
 # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
@@ -11,7 +27,7 @@ try:
     observation = owm.weather_at_place('London,GB')
 except pyowm.exceptions.unauthorized_error.UnauthorizedError as e:
     print(f"Something went wrong:\n {e}")
-    exit()
+    sys.exit(6)
 
 w = observation.get_weather()
 print(w)                      # <Weather - reference time=2013-12-18 09:20,
